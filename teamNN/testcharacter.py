de7ab758f7f9ca7366a1e480c638833e1384bbcd
RobertGunduz <rgunduz@wpi.edu>
2025-02-05 14:44:31 -0500

    Update testcharacter.py
    Added in  bunch of code for Heuristics, Neighbor finding, and preliminary A star. Looking to move away from Precalculated H

--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -14,17 +14,101 @@ class TestCharacter(CharacterEntity):
         # Your code here
         pass
     
-    # Find movement difficulty of terrain given
+    # Find movement difficulty of terrain given in grid form
     def MapCheck(self, wrld:World):
         W_width = wrld.width()
         W_height = wrld.height()
+        Exit_x = 0
+        Exit_y = 0
         Hx = [W_width][W_height]
-        Epos = wrld.exit_at()
+        # Find the Exit Position on the Grid
         for i in range(W_width):
             for j in range(W_height):
-                pass
+                if wrld.exit_at(i, j):
+                    Exit_x = i
+                    Exit_y = j
+                    break
+        # Calculate Movement difficulty (Wall Presence + Manhattan dist)
+        for i in range(W_width):
+            for j in range(W_width):
+                # Manhattan Distance
+                Hx[i][j] = abs(i - Exit_x) + abs(j - Exit_y)
+                # Check for Wall
+                if wrld.wall_at(i, j):
+                    Hx[i][j] += 50
+        return Hx
+
+    def neighbors(self, wrld, x, y):
+        # List of empty cells
+        cells = []
+        # Go through neighboring cells
+        for dx in [-1, 0, 1]:
+            # Avoid out-of-bounds access
+            if ((x + dx >= 0) and (x + dx < wrld.width())):
+                for dy in [-1, 0, 1]:
+                    # Avoid out-of-bounds access
+                    if ((y + dy >= 0) and (y + dy < wrld.height())):
+                        # Is this cell safe?
+                        if(wrld.exit_at(x + dx, y + dy) or
+                           wrld.empty_at(x + dx, y + dy)):
+                            # Yes
+                            cells.append((dx, dy))
+        # All done
+        return cells
+
+    # Manhattan Distance Helper Function
+    def heuristic(self, x1, y1, x2, y2):
+        return (abs(x1 - x2) + abs(y1 - y2))
+
+    def next_move(self, wrld:World):
+        start = (self.x, self.y)
+        Exit_x = 0
+        Exit_y = 0
+        for i in range(wrld.width()):
+            for j in range(wrld.height()):
+                if wrld.exit_at(i, j):
+                    Exit_x = i
+                    Exit_y = j
+                    break
+
+        frontier = [start]
+        explored = []
+        came_from = []
+        g_count = {start: 0}
+        f_count = {start: heuristic(self.x, self.y, Exit_x, Exit_y)}
+
+        while frontier:
+            curr = min(frontier, key=lambda x: f_count[x])
+
+            if curr == goal:
+                pass ## TODO ADD PATH RECONSTRUCTION
+            
+            frontier.remove(curr)
+            explored.append(curr)
+
+            for neighbor in neighbors(current):
+                if neighbor in explored:
+                    continue
+
+                t_g_count = g_count[curr] + heuristic(curr[0], curr[1], neighbor[0], neighbor[1])
+
+                if neighbor not in frontier:
+                    frontier.append(neighbor)
+                elif t_g_count >= g_count[neighbor]:
+                    continue
+
+                came_from[neighbor] = curr
+                g_count[neighbor] = t_g_count
+                f_count[neighbor] = g_count[neighbor] + heuristic(neighbor[0], neighbor[1], Exit_x, Exit_y)
+
                 
 
+
+
+
+
+                
+    
 
     # Move this to a separate file later, base code from: https://www.redblobgames.com/pathfinding/a-star/introduction.html
     def A_Star(self, wrld:World):
