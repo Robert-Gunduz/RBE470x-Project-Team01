73e1d54a021fdc00537054ead1d30b9ca38230af
RobertGunduz <rgunduz@wpi.edu>
2025-02-06 17:32:59 -0500

    Cleaned Code and Chebyshev
    Added Comments to Code and included Chebyshev distance calculation

--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -10,10 +10,10 @@ from colorama import Fore, Back
 
 class TestCharacter(CharacterEntity):
 
+    # Runs when it is this Character's turn 
     def do(self, wrld):
-        # Your code here
-        (dx, dy) = self.next_move(wrld)
-        print(dx, dy)
+        # put AI-behavior code HERE:
+        (dx, dy) = self.next_move(wrld) # A-Star finds next move
         self.move(dx - self.x, dy - self.y)
     
     # Find movement difficulty of terrain given in grid form
@@ -40,6 +40,7 @@ class TestCharacter(CharacterEntity):
                     Hx[i][j] += 50
         return Hx
 
+    # Function to check for valid neighboring Cells, returns list of coordinates (modified from 'look_for_empty_cell')
     def neighbors(self, wrld, x, y):
         # List of empty cells
         cells = []
@@ -58,23 +59,26 @@ class TestCharacter(CharacterEntity):
         # All done
         return cells
 
-    # Manhattan Distance Helper Function
+    # Distance Helper Function (choose one provided by comment/uncomment)
     def heuristic(self, x1, y1, x2, y2):
-        return (abs(x1 - x2) + abs(y1 - y2))
+        #return max(abs(x2-x1), abs(y2-y1)) # Chebyshev
+        #return (abs(x1 - x2) + abs(y1 - y2)) # Manhattan
+        return pow(pow((x1 - x2), 2) + pow((y1 - y2), 2), 0.5) # Pythagorean
 
     # reconstructs path in stack form
     def trace_path(self, came_from, current):
-
         path = []
         while current in came_from:
             path.append(current)
             current = came_from[current]
         return path
 
+    # returns the next move coordinates suggested by A-Star
     def next_move(self, wrld:World):
         start = (self.x, self.y)
         Exit_x, Exit_y = 0, 0
 
+        #Find the goal in the World space
         for i in range(wrld.width()):
             for j in range(wrld.height()):
                 if wrld.exit_at(i, j):
@@ -84,6 +88,7 @@ class TestCharacter(CharacterEntity):
 
         goal = (Exit_x, Exit_y)
 
+        # "Tables" to record frontier, visited nodes, and heuristic values
         frontier = []
         frontier.append(start)
         explored = set()
@@ -91,58 +96,39 @@ class TestCharacter(CharacterEntity):
         g_count = {start: 0}
         f_count = {start: self.heuristic(self.x, self.y, Exit_x, Exit_y)}
 
+        # A-Star Loop
         while frontier:
+            # Use node in frontier with smallest F value
             curr = min(frontier, key=lambda x: f_count[x])
-            
 
+            # Start path tracing if current node is the goal
             if curr == goal:
-                print("Goal Found")
                 path = self.trace_path(came_from, curr)
-                path.pop() # Pop start node
-                return path.pop()
+                path.pop() # Pop start node (currently occupied by character)
+                return path.pop() # return the next node (suggested next move from path)
             
+            # Move Current node to visited nodes
             frontier.remove(curr)
             explored.add(curr)
 
+            # check valid neighboring nodes (Validation controlled in accompanying helper)
             for neighbor in self.neighbors(wrld, curr[0], curr[1]):
+                # skip node if already visited
                 if neighbor in explored:
                     continue
-
+                
+                # current g value calculation
                 t_g_count = g_count[curr] + self.heuristic(curr[0], curr[1], neighbor[0], neighbor[1])
 
+                # check if neigbor is in frontier already or if better t value has been found
                 if neighbor not in frontier:
                     frontier.append(neighbor)
-                    print(neighbor)
                 elif t_g_count >= g_count[neighbor]:
                     continue
-
+                
+                # Add node to the came_from dictionary, G values, and F values for frontier search and path reconstruction
                 came_from[neighbor] = curr
                 g_count[neighbor] = t_g_count
                 f_count[neighbor] = g_count[neighbor] + self.heuristic(neighbor[0], neighbor[1], goal[0], goal[1])
-                print(f_count[neighbor])
         
-        return None
-
-    # Move this to a separate file later, base code from: https://www.redblobgames.com/pathfinding/a-star/introduction.html
-    def A_Star(self, wrld:World):
-        frontier = PriorityQueue()
-
-        frontier.put(start, 0)
-        came_from = dict()
-        cost_so_far = dict()
-        came_from[start] = None
-        cost_so_far[start] = 0
-
-        while not frontier.empty():
-            current = frontier.get()
-
-            if current == goal:
-                break
-            
-            for next in graph.neighbors(current):
-                new_cost = cost_so_far[current] + graph.cost(current, next)
-                if next not in cost_so_far or new_cost < cost_so_far[next]:
-                    cost_so_far[next] = new_cost
-                    priority = new_cost + heuristic(goal, next)
-                    frontier.put(next, priority)
-                    came_from[next] = current
+        return None # Return Nonetype if no valid path has been found to the End Position
